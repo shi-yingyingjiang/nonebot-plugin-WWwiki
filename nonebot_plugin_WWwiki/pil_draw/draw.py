b34a2e1f7355b12aae10b32922eec76086d4084e
SuperGuGuGu <13680478000@163.com>
2024-11-29 17:19:00 +0800

    feat: 添加绘制相关代码

--- nonebot_plugin_WWwiki/pil_draw/draw.py
+++ nonebot_plugin_WWwiki/pil_draw/draw.py
@@ -2,19 +2,107 @@
 from PIL import Image
 from nonebot import logger, require
 
+from .config import draw_color
+
 require("nonebot_plugin_htmlrender")
 from nonebot_plugin_htmlrender import template_to_pic
 
-from .tools import save_image
+from .tools import save_image, load_image, draw_text
 
 
-async def draw_main(draw_data: dict, template_name: None):
-    logger.debug(f"template_name: {template_name}")
+async def draw_main(draw_data: dict, temp_name: str = None, temp_path: str = None):
     logger.debug(f"draw_data: {draw_data}")
-    image = Image.new("RGBA", (100, 100), (50, 50, 50, 255))
+    template_name = temp_path.split("/")[-1]
+    logger.debug(f"template_path: {temp_path}")
+    logger.debug(f"template_name: {template_name}")
+
+    # 鸣潮角色查询
+    if template_name == "rolecard":
+        return await draw_rolecard(draw_data)
+    # 鸣潮技能查询
+    elif template_name == "":
+        return await draw_name(draw_data)
+    # 鸣潮共鸣链查询
+    elif template_name == "":
+        return await draw_name(draw_data)
+    # 鸣潮
+    elif template_name == "":
+        return await draw_name(draw_data)
+    # 鸣潮
+    elif template_name == "":
+        return await draw_name(draw_data)
+    # 鸣潮
+    elif template_name == "":
+        return await draw_name(draw_data)
+    # 鸣潮
+    elif template_name == "":
+        return await draw_name(draw_data)
+
+    img = await template_to_pic(
+        temp_path,
+        temp_name,
+        draw_data,
+    )
+    return img
+
+
+async def draw_rolecard(draw_data: dict):
+    """
+    绘制
+    """
+    image_x, image_y = (900, 0)
+    image_y += 643  # 基础信息界面
+
+    image_y += 0  # 内容栏
+
+    image_y += 50  # 底部留空
+    image = Image.new("RGBA", (image_x, image_y), (50, 50, 50, 255))
+
+    # 开始绘制
+    # 立绘
+    paste_image = await load_image(draw_data.get("roleimg"))
+    image.alpha_composite(paste_image, (263, -57))
+
+    # 图标
+    paste_image = await draw_text(
+        "鸣潮WIKI",
+        size=36,
+        textlen=99,
+        fontfile="优设好身体.ttf",
+        text_color=draw_color("图标"),
+        calculate=False
+    )
+    image.alpha_composite(paste_image, (25, 26))
+
+    # 副图标
+    paste_image = await draw_text(
+        "wuthering waves",
+        size=19,
+        textlen=99,
+        fontfile="优设好身体.ttf",
+        text_color=draw_color("副图标"),
+        calculate=False
+    )
+    image.alpha_composite(paste_image, (17, 60))
+
+    # 标题
+    paste_image = await draw_text(
+        draw_data.get("title"),
+        size=72,
+        textlen=99,
+        fontfile="优设好身体.ttf",
+        text_color=draw_color("标题"),
+        calculate=False
+    )
+    image.alpha_composite(paste_image, (38, 150))
 
     return save_image(image, to_bytes=True)
 
 
-async def draw_name():
-    return
+async def draw_name(draw_data: dict):
+    """
+    绘制
+    """
+    image = Image.new("RGBA", (100, 100), (50, 50, 50, 255))
+
+    return save_image(image, to_bytes=True)
